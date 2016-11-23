5f17d2e0893b383996eb871f8196f4b45af176b4
Adrian Ottiker <adrian@ottiker.com>
2016-11-23 19:21:26 +0100

    zip readable streams

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const zlib = require("zlib");
+
 exports.gzip = (scope, inst, args, data, next) => {
+    data.readable = data.readable.pipe(zlib.createGzip(args.options));
     next(null, data);
 };
